86bd0cbfa71038c89de919eb722d6f894d42e6cb
rahultiwari8685 <rtiwari69572@gmail.com>
2026-06-17 11:38:49 +0530

    size increase

--- backend/src/server.js
+++ backend/src/server.js
@@ -62,7 +62,13 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json({ limit: "1mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "50mb",
+  }),
+);
 
 // app.use("/uploads", express.static("uploads"));
 
