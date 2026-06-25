894793dac5f8ce2abe7d36d6218440cc7e0584f1
rahultiwari8685 <rtiwari69572@gmail.com>
2026-06-25 18:57:54 +0530

    added

--- admin/src/views/pages/login/Login.js
+++ admin/src/views/pages/login/Login.js
@@ -205,14 +205,6 @@ const styles = {
     lineHeight: '1.8',
   },
 
-  rightPane: {
-    width: '70%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: '40px',
-  },
-
   card: {
     width: '100%',
     maxWidth: '430px',
